2605d6455e597ddbd3c097a8f4b507ce73739624
Derek Gooding <djgooding@live.com>
2025-12-01 11:26:35 -0500

    Castle maps

---
+++ 0-Bit Legend/Maps/Castle0.cs
@@ -0,0 +1,44 @@
+﻿namespace _0_Bit_Legend.Maps;
+
+public class Castle0 : IMap
+{
+    public string Name => "Castle 0";
+    public string[] Raw =>[
+"======================================================================================================#",
+"=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=#",
+"=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=#",
+"=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=#",
+"=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=#",
+"=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX==========================================XXXXXXXXXXXXXXXXXXXXXXXXXXXX=#",
+"=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=                                        =XXXXXXXXXXXXXXXXXXXXXXXXXXXX=#",
+"=XXXXXXXXXXXXXXXXXXXXXXXX=======                                        =============XXXXXXXXXXXXXXXX=#",
+"=XXXXXXXXXXXXXXXXXXXXXXXX=                             |***                         =XXXXXXXXXXXXXXXX=#",
+"=XXXXXXXXX================                             |******                      ==================#",
+"=XXXXXXXXX=                                            |  ****                                        #",
+"=XXXXXXXXX=                                            =                                              #",
+"=XXXXXXXXX=                             = == =         = = == =                                       #",
+"=XXXXXXXXX=                             ======         = ======                                       #",
+"=XXXXXXXXX=           XXXXX             ||||||===========||||||             XXXXX                     #",
+"=XXXXXXXXX=           =/X/=             =======/////////=======             =/X/=                     #",
+"=XXXXXXXXX=           =====             ||||||=/////////=||||||             =====                     #",
+"=XXXXXXXXX=                             =======/////////=======                                       #",
+"=XXXXXXXXX=                                                                                           #",
+"=XXXXXXXXX=                                                                                           #",
+"=XXXXXXXXX=                                                                                           #",
+"=XXXXXXXXX=                                                                                           #",
+"=XXXXXXXXX=====                                                                                       #",
+"=XXXXXXXXXXXXX=                                                                                       #",
+"=XXXXXXXXXXXXX=                                                                                       #",
+"=XXXXXXXXXXXXX=                              ==============                                           #",
+"=XXXXXXXXXXXXX================================XXXXXXXXXXXX=                        ===================#",
+"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX==========================XXXXXXXXXXXXXXXXX=#",
+"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=#",
+"=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=#",
+"=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=#",
+"=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=#",
+"======================================================================================================#",
+];
+    public string[] FlagAdjusted => Raw;
+
+    public void Load() { }
+}
