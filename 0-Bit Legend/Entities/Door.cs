d2180c3190be671e819d76e649d3fa27701dd7f5
Derek Gooding <djgooding@live.com>
2025-12-02 18:09:57 -0500

    Prep for armor/door/raft entities

---
+++ 0-Bit Legend/Entities/Door.cs
@@ -0,0 +1,10 @@
+﻿namespace _0_Bit_Legend.Entities;
+
+public class Door : IEntity
+{
+    public Vector2 Position { get; set; } = Vector2.Zero;
+    public DirectionType Direction { get; set; }
+    public void Draw(int posX, int posY, DirectionType direction) => throw new NotImplementedException();
+    public bool IsTouching(int posX, int posY, char[] symbols) => throw new NotImplementedException();
+    public bool IsTouching(int posX, int posY, char symbol) => throw new NotImplementedException();
+}
