27fc28b513cd54cbc131aec14d2360b7035142e1
Derek Gooding <djgooding@live.com>
2025-12-08 10:24:31 -0500

    sword sprites

--- 0-Bit Legend/Entities/SwordAttack.cs
+++ 0-Bit Legend/Entities/SwordAttack.cs
@@ -2,40 +2,37 @@
 
 public class SwordAttack : IEntity, ICollider
 {
+    public bool IsActive { get; set; } = true;
     public Vector2 Position { get; set;  } = Vector2.Zero;
     public DirectionType Direction { get; set; } = DirectionType.Up;
 
-    public Vector2 Size { get; } = new(0,0);
+    public Vector2 Size { get; } = new(2,2);
 
     private readonly Dictionary<DirectionType, string[]> _spriteSheet = new()
     {
         { DirectionType.Up,
         [
-            " ___ ",
-            "| = |",
-            "|^ ^|",
-           @" \_/ ",
+            " S ",
+            " S ",
+            "---",
         ]},
         { DirectionType.Left,
         [
-           @"  /\ ",
-            " /  |",
-            "|^  |",
-            "|_=_|",
+            "  |",
+            "SS|",
+            "  |",
         ]},
         { DirectionType.Down,
         [
-            "  _  ",
-           @" / \ ",
-            "|^ ^|",
-            "| = |",
+            "---",
+            " S ",
+            " S ",
         ]},
         { DirectionType.Right,
         [
-           @" /\  ",
-           @"|  \ ",
-            "|  ^|",
-            "|_=_|",
+            "|  ",
+            "|S ",
+            "|  ",
         ] },
     };
 
